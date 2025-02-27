5f2f722bae31dfa39916f1f6fcc069ff64ef01dd
sadism <gsadism@163.com>
2025-02-27 22:18:36 +0800

    secret

--- cmd/command.go
+++ cmd/command.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"crypto/aes"
 	"fmt"
 	"github.com/gsadism/open-admin/core/debug"
+	"github.com/gsadism/open-admin/pkg/crypto/_aes"
 	"github.com/gsadism/open-admin/pkg/crypto/passlib"
 	"github.com/spf13/cobra"
 )
@@ -28,6 +30,21 @@ func OpenAdminSuper() *cobra.Command {
 	return c
 }
 
+func OpenAdminSecret() *cobra.Command {
+	c := &cobra.Command{
+		Use:  "secret",
+		Args: cobra.ArbitraryArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			if key, err := _aes.Secret(2 * aes.BlockSize); err != nil {
+				debug.Error(err.Error())
+			} else {
+				debug.Debug(fmt.Sprintf("SECRET_KEY: %s", key))
+			}
+		},
+	}
+	return c
+}
+
 func OpenAdminServer() *cobra.Command {
 	var ConfigFilePath string
 
@@ -41,7 +58,7 @@ func OpenAdminServer() *cobra.Command {
 
 	c.PersistentFlags().StringVarP(&ConfigFilePath, "config", "c", "", "config file path")
 
-	c.AddCommand(OpenAdminSuper())
+	c.AddCommand(OpenAdminSuper(), OpenAdminSecret())
 
 	return c
 }
